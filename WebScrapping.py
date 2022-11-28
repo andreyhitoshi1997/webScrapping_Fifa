bc2529c0745f62e98ec33073294493f251ae5b1c
andrey <andrey.onoue@banqi.com>
2022-11-28 15:45:18 -0300

    Subido v2

--- WebScrapping.py
+++ WebScrapping.py
@@ -2,15 +2,15 @@ import requests
 from time import sleep
 from datetime import datetime
 
-def get_match_data():
+def get_match_data(firstCountry, secondCountry):
     return requests.get(
-        url = 'https://temporeal.lance.com.br/storage/matches/copa-do-mundo-2022-28-11-2022-brasilxsuica.json'
+        url = f'https://temporeal.lance.com.br/storage/matches/copa-do-mundo-2022-28-11-2022-{firstCountry}x{secondCountry}.json'
     ).json()
 
 last_update = None
 
 while True:
-    match_data = get_match_data()
+    match_data = get_match_data('portugal', 'uruguai')
 
     narrations = match_data['match']['narrations']
     last_narration = narrations[len(narrations)-1]
